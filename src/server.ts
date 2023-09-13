b0da3ba5eddce851f35f2e64f39aee70939677c6
WilliamWJD <william.jose.wjd@gmail.com>
2023-09-13 10:15:23 -0300

    configurando prisma e rotas

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import { fastify } from "fastify";
+import { getAllPromptsRoutes } from "./routes/get-all-prompts";
 
 const app = fastify();
 
-app.get("/", () => {
-  return "Hello World";
-});
+app.register(getAllPromptsRoutes);
 
 app
   .listen({
